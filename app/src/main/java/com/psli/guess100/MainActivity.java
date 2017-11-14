0044a769b03d2cd66f72456ec51d02f49233c80e
psli0101 <p.s.li19980101@gmail.com>
2017-11-14 10:17:37 +0800

    Guess

--- app/src/main/java/com/psli/guess100/MainActivity.java
+++ app/src/main/java/com/psli/guess100/MainActivity.java
@@ -1,6 +1,7 @@
 package com.psli.guess100;
 
 import android.opengl.ETC1;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,19 +9,47 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    Random random = new Random();
+    int sec_num = random.nextInt(100)+1;
+    int start = 0;
+    int end = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        findViews(); 
+        findViews();
     }
 
     public void findViews() {
         TextView t_secret = (TextView) findViewById(R.id.secret);
-        TextView t_info = (TextView) findViewById(R.id.info);
-        EditText ed_number = (EditText) findViewById(R.id.number);
+        t_secret.setText(Integer.toString(sec_num));
+        final TextView t_info = (TextView) findViewById(R.id.info);
+        final EditText ed_number = (EditText) findViewById(R.id.number);
         Button b_send = (Button) findViewById(R.id.send);
+        b_send.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        int num = Integer.parseInt(ed_number.getText().toString());
+                        if (num == sec_num) {
+                            new AlertDialog.Builder(MainActivity.this)
+                                    .setMessage("Right Answer")
+                                    .setPositiveButton("OK", null)
+                                    .show();
+                        } else {
+                            if(num > sec_num) {
+                                end = num;
+                            } else if(num < sec_num) {
+                                start = num;
+                            }
+                            t_info.setText(start+" to 88"+end);
+                        }
+                    }
+                }
+        );
     }
 }
