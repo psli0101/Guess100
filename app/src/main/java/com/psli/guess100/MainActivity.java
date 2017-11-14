cca900c66f2ce43bea386f22fe297f78098a98a2
psli0101 <p.s.li19980101@gmail.com>
2017-11-14 09:58:28 +0800

    Layout

--- app/src/main/java/com/psli/guess100/MainActivity.java
+++ app/src/main/java/com/psli/guess100/MainActivity.java
@@ -1,7 +1,12 @@
 package com.psli.guess100;
 
+import android.opengl.ETC1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+    }
+
+    public void findViews() {
+        TextView t_secret = (TextView) findViewById(R.id.secret);
+        TextView t_info = (TextView) findViewById(R.id.info);
+        EditText ed_number = (EditText) findViewById(R.id.number);
+        Button b_send = (Button) findViewById(R.id.send);
     }
 }
